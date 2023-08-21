67d9483d5782b369d0109a24c23e353e05f46560
maqiie <candiemp2580@gmail.com>
2023-08-21 04:51:09 +0300

    creates users and signs them in

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+      
+        skip_before_action :verify_authenticity_token
+
+        include DeviseTokenAuth::Concerns::SetUserByToken
+
+        
+end
